c25569e4543da22becf9bd7571c81cd677a895a9
HarrySeop <parkpjs3083@naver.com>
2023-08-29 09:58:07 +0900

    2023.03.08 OpenGL 첫 시작
    OpenGL을 이용해서 네모를 만들어봤습니다.

---
+++ Chap01/sample001/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <gl/glut.h>
+#include <gl/glu.h>
+
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.5, 0.5, 0.5);
+	glBegin(GL_POLYGON);
+		glVertex3f(-0.5, -0.5, 0.0);
+		glVertex3f(0.5, -0.5, 0.0);
+		glVertex3f(0.5, 0.5, 0.0);
+		glVertex3f(-0.5, 0.5, 0.0);
+	glEnd();
+	glFlush();
+}
+int main() {
+	glutCreateWindow("OpenGL Initialize Test");
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
+	return 0;
+}
